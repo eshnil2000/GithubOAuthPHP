87a03a950cdd5afd3ac0f116cf9a5f52e0624918
Nick <eshnil2000@gmail.com>
2018-05-12 14:09:55 -0700

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require "init.php";
 }*/
 
 /* Redirect browser */
-/*header("Location: http://54.245.32.80/redir/index2.html"); */
+/*header("Location: http://localhost/redir/index2.html"); */
  
 /* Make sure that code below does not get executed when we redirect. */
 /*exit;*/
